80877b1053529d62979ed58fb2a35ff75ba807c6
Bert Verstraete <bertverstraete22@gmail.com>
2019-10-29 13:43:54 +0100

    Update reeks-9-array.asm

--- reeks-9-array.asm
+++ reeks-9-array.asm
@@ -1,34 +1,42 @@
 $include(c8051f120.inc)
 
-cseg at 0000H
-      jmp main
+cseg at 000H
+	jmp main
+
 cseg at 0050H
-main: clr EA
-      mov WDTCN, #0DEH
-      mov WDTCN, #0ADH
-      setb EA
-      mov SFRPAGE,#0EH
-      mov XBR2,#40H
-      mov P1MDOUT,#0FFH
-      mov 20H,#01001000b  ; bitpatroon 0 ARRAY  IN ASM BOUWEN
-      mov 21H,#01001000b   ; bitpatroon 1
-      mov 22H,#01001000b
-      mov 23H,#01001000b
-      mov 24H,#01001000b
-      mov 25H,#01001000b
-      mov 26H,#01001000b
-      mov 27H,#01001000b
-      mov 28H,#01001000b
-      mov 29H,#01001000b
 
-start: jb P3.7,$
-       jnb P3.7,$
-       inc R2
-       cjne R2,#10d, schrijf ; compare jump not equal(x, y, callback) ; R2 is teller 
-       mov R2,#00d
+main: 
+	clr EA
+	mov WDTCN,#0DEH
+	mov WDTCN,#0ADH
+	setb EA
 
-schrijf: mov A, #20H
-	add A, R2 (R0 als pointer, (tab+idx))
+	mov SFRPAGE,#0FH
+	mov XBR2,#40H
+	mov P1MDOUT,#0FFH
+	mov 20H,#3FH ;bitpatroon
+	mov 21H,#06H
+	mov 22H,#5BH
+	mov 23H,#4FH
+	mov 24H,#66H
+	mov 25H,#6DH
+	mov 26H,#7DH
+	mov 27H,#07H
+	mov 28H,#7FH
+	mov 29H,#6FH
+	
+	mov R2,#00H; teller
+
+start:	
+	jb P3.7,$
+	jnb P3.7,$
+	inc R2
+	cjne R2,#10d,schrijf ;compare and jump if not equal
+	mov R2,#00H
+
+schrijf:
+	mov A,#20H
+	add A,R2
 	mov R0,A
-	mov P1,@R0 ; p1 => *(tab + R2) (WAARDE VAN R0 NAAR P1 WESGSCHRIJVEN)
+	mov P1,@R0 ;equivalent voor * in c(R0 is een pointer)
 	jmp start
